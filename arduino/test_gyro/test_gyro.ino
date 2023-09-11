93174abe02c8b59f62baeb8e23e607040bb0ac68
HoneyMack <tosimiti7@gmail.com>
2023-09-11 23:06:49 +0900

    ファイル名の変更

--- arduino/test_gyro/test_gyro.ino
+++ arduino/test_gyro/test_gyro.ino
@@ -1,8 +1,8 @@
 /*
   Arduino LSM6DS3 - Simple Gyroscope
 
-  This example reads the gyroscope values from the LSM6DS3 sensor 
-  and prints them to the Serial Monitor or Serial Plotter, as a directional detection of 
+  This example reads the gyroscope values from the LSM6DS3 sensor
+  and prints them to the Serial Monitor or Serial Plotter, as a directional detection of
   an axis' angular velocity.
 
   The circuit:
@@ -35,36 +35,32 @@ void setup() {
   }
   Serial.print("Gyroscope sample rate = ");
   Serial.print(IMU.gyroscopeSampleRate());
-  Serial.println(" Hz");  
+  Serial.println(" Hz");
   Serial.println();
   Serial.println("Gyroscope in degrees/second");
 }
 
 
 void loop() {
-  
+
   if (IMU.gyroscopeAvailable()) {
     IMU.readGyroscope(x, y, z);
   }
-  if(y > plusThreshold)
-  {
+  if (y > plusThreshold) {
     Serial.println("Collision front");
     delay(500);
   }
-  if(y < minusThreshold)
-  {
+  if (y < minusThreshold) {
     Serial.println("Collision back");
     delay(500);
   }
-  if(x < minusThreshold)
-  {
+  if (x < minusThreshold) {
     Serial.println("Collision right");
     delay(500);
   }
-    if(x > plusThreshold)
-  {
+  if (x > plusThreshold) {
     Serial.println("Collision left");
     delay(500);
   }
-  
+
 }
